3a0f1b94f285f06449efaa49d1bf09bab296bf1b
Diogo Silva <31267895+realdiogosilva@users.noreply.github.com>
2022-08-25 09:25:35 -0700

    test

---
+++ interface.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, Response
+from functools import wraps
+import psycopg2
+import psycopg2.extras
+import datetime
+import os
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import ssl
+import urllib.request
+
+
+@app.route('/')
+def mainidea():
+    return render_template('frontpage.html')
+
+
