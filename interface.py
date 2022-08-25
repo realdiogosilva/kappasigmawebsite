73bfd5eb5f021d1e803fa841ac80c00a3bc45326
Diogo Silva <31267895+realdiogosilva@users.noreply.github.com>
2022-08-25 09:30:30 -0700

    test

--- interface.py
+++ interface.py
@@ -9,9 +9,36 @@ from urllib.request import Request, urlopen
 import ssl
 import urllib.request
 
+app = Flask(__name__)
+
+connection_string = os.environ['DATABASE_URL']
+
+def check_auth(username, password):
+    '''THIS IS FOR THE USERNAME AND PASSWORD VARIABLE'''
+    return username == os.environ['USERNAME']; password == os.environ['PASSWORD'];
+
+def authenticate():
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 def mainidea():
     return render_template('frontpage.html')
+
+@app.route('/daprs202grades')
+@requires_auth
+def hello_world():
+        return render_template('grades.html')
 
 
